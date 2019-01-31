c1374ab293c31070928fd8a2facd26655ee8ca57
Laura Gillespie <lauragillespie@lauras-mbp.home>
2019-01-31 17:47:38 -0500

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+// an array that holds all of the friends data
+var friendsData = [
+  {
+    name: "Jon Snow",
+    photo:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/john-snow-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+    scores: [5, 4, 3, 5, 2, 5, 2, 4, 5, 2]
+  },
+  {
+    name: "Han Solo",
+    photo:
+      "https://media.wired.com/photos/5a99c6e12b62655fe349d3fc/master/pass/HanSolo.png",
+    scores: [3, 3, 4, 5, 2, 5, 4, 2, 3, 3]
+  },
+  {
+    name: "Serena Van Der Woodsen",
+    photo:
+      "https://vignette.wikia.nocookie.net/gossipgirl-megd/images/2/2d/Serena_van_der_Woodsen.jpg/revision/latest?cb=20150607191654",
+    scores: [4, 1, 1, 4, 1, 1, 1, 5, 2, 3]
+  },
+  {
+    name: "Lorelai Gilmore",
+    photo:
+      "http://4.bp.blogspot.com/-K0ImC5HfkZ4/VJPuA_1dPOI/AAAAAAAAPS8/pyWvc9i3pxA/s1600/Lorelai-Gilmore-lorelai-gilmore-24368445-1024-768.png",
+    scores: [3, 2, 5, 5, 5, 3, 4, 5, 2, 4]
+  },
+  {
+    name: "Rey",
+    photo:
+      "https://lumiere-a.akamaihd.net/v1/images/talj-rey-main-image-2_668bb5dc.jpeg?region=0%2C0%2C1560%2C878&width=960",
+    scores: [2, 3, 2, 5, 2, 5, 3, 1, 2, 5]
+  },
+  {
+    name: "Bruce Wayne",
+    photo:
+      "https://images.forbes.com/media/lists/fictional/2011/bruce-wayne_197x282.jpg",
+    scores: [4, 5, 3, 2, 2, 1, 2, 3, 3, 3]
+  }
+];
+
+module.exports = friendsData;
